5786f75d27acf65d3a040c9b7b3915d63ec8066e
rishabh <rishabhr264@gmail.com>
2018-07-24 09:58:43 +0530

    Initial commit

---
+++ src/main/java/com/scgj/SDMS/SdmsApplication.java
@@ -0,0 +1,12 @@
+package com.scgj.SDMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SdmsApplication.class, args);
+	}
+}
