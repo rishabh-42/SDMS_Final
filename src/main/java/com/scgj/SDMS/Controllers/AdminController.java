332eea703b2184a5467b010a542a21821e70ec86
rishabh <rishabhr264@gmail.com>
2018-07-24 18:25:31 +0530

    images added in PL

---
+++ src/main/java/com/scgj/SDMS/Controllers/AdminController.java
@@ -0,0 +1,18 @@
+package com.scgj.SDMS.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class AdminController {
+
+ @RequestMapping("/getPlacementReport")
+    @ResponseBody
+public ModelAndView getPlacementReport ()
+{
+    ModelAndView m = new ModelAndView("placementReport");
+    return m;
+}
+}
